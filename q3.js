938a849e4afb4a0da54a6765f15ab0e8e0084724
sanjaykumar06-10 <thallapallisanjaykumar06@gmail.com>
2025-05-29 18:47:17 +0530

    update

---
+++ q3.js
@@ -0,0 +1,20 @@
+db.products.insertMany([
+  {
+    name: "Mouse",
+    description: "wireless mouse with USB receiver.",
+    imgUrl: "https://example.com/mouse.jpg",
+    price: 1500
+  },
+  {
+    name: " Keyboard",
+    description: "RGB keyboard.",
+    imgUrl: "https://example.com/keyboard.jpg",
+    price: 1950
+  },
+  {
+    name: "Monitor",
+    description: "24-inch full HD monitor .",
+    imgUrl: "https://example.com/monitor.jpg",
+    price: 10000
+  }
+]);
